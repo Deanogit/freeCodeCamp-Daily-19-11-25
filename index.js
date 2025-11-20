b1bebd82c7b2d433d463968ed688b8ee4007f093
Dean Howe <hideanhowe@gmail.com>
2025-11-20 15:07:35 +0000

    regex

--- index.js
+++ index.js
@@ -17,30 +17,29 @@
 
 function convert(heading) {
   //take in the heading and split it up
-  const array = heading.split(' ');
+  // const array = heading.split(' ');
   // does array.include "level"
-  console.log(array.includes('level'));
-  if (array.includes('level')) {
-    // look at the array[0]
-    // console.log(array[0])
-    //// does it contain all #s?
-    if (array[0].includes('#')) {
-      //console.log("true")
-      ////// if no return "Invalid format"
-      ////// if yes
-      //////// what is the length of array[0]?
-      //console.log(array[0].length)
-      let length = array[0].length;
-      ////////// if its between 1-6
-      //////////// store length in a variable
-      if (length >= 1 && length <= 6) {
-        return `<h${length}>${array.slice(1).join(' ')}</h${length}>`;
-      } else return 'Invalid format';
-    } else return 'Invalid format';
-  } else return 'Invalid format';
+  // console.log(array.includes('level'));
+  // if (array.includes('level')) {
+  // look at the array[0]
+  // console.log(array[0])
+  //// does it contain all #s?
+  // if (array[0].includes('#')) {
+  //console.log("true")
+  ////// if no return "Invalid format"
+  ////// if yes
+  //////// what is the length of array[0]?
+  //console.log(array[0].length)
+  // let length = array[0].length;
+  ////////// if its between 1-6
+  //////////// store length in a variable
+  // if (length >= 1 && length <= 6) {
+  //  return `<h${length}>${array.slice(1).join(' ')}</h${length}>`;
+  // } else return 'Invalid format';
+  // } else return 'Invalid format';
+  // } else return 'Invalid format';
   //////////// return `<h${variable}>` + remainder of array (pop first entry? and loop through with spaces added?)
   //////////// else return Invalid format
-
   // const pattern = /#/g
   // let result = heading.match(pattern)
   // console.log(result)
@@ -48,3 +47,22 @@ function convert(heading) {
   //  console.log(heading)
   // }
 }
+
+// const regex = //
+// start with zero or more spaces
+// const regex = /^\s*/
+////
+// start with zero or more spaces
+// 1 to 6 hash characters (#) in a row
+// const regex = /^[\s]*\#{1,6}/
+////
+// start with zero or more spaces
+// 1 to 6 hash characters (#) in a row
+// At least one space
+// const regex = /^[\s]*\#{1,6}[\s]+/
+////
+// start with zero or more spaces
+// 1 to 6 hash characters (#) in a row
+// At least one space
+// The heading text.
+// const regex = /^[\s]*\#{1,6}[\s]+[a-zA-Z]+/
